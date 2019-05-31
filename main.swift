ebe87ce7f4a81961fb6d4c76a5c6743ed87408e8
Sam Pettersson <sam@sampettersson.com>
2019-05-31 14:28:43 +0200

    Make all replacements camelCased even when they are not

--- main.swift
+++ main.swift
@@ -14,7 +14,7 @@ let swiftTranslationsCodegenVersionCheckPath = "\(cwd)/.swiftTranslationsCodegen
 
 if FileManager.default.fileExists(atPath: swiftTranslationsCodegenVersionCheckPath) {
     let requiredVersion = try String(contentsOf: URL(fileURLWithPath: swiftTranslationsCodegenVersionCheckPath), encoding: .utf8).trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
-    
+
     if requiredVersion != currrentVersion && CommandLine.arguments.index(of: "--ignore-versions") == nil {
         print("\(colorRed)Project requires version '\(requiredVersion)' but you are running '\(currrentVersion)' adjust '.swiftTranslationsCodegen' or pass '--ignore-versions'")
         exit(1)
@@ -26,17 +26,17 @@ let placeholderKeyRegex = try NSRegularExpression(pattern: "([a-zA-Z0-9_]+)")
 
 if CommandLine.arguments.index(of: "--help") != nil {
     print("""
-        \(colorWhite)Hedvig Translations Codegen
-        
-        Arguments:
-        \(colorWhite)--projects: The projects you want to fetch translations for (for example: "[App, IOS]") \u{001B}[0;31mREQUIRED
-        \(colorWhite)--destination: Full path of desired destination for generated Swift file (including ".swift", for example "translations/translations.swift") \(colorRed)REQUIRED
-        \(colorWhite)--swiftformat-path: The path to the Swiftformat CLI \(colorYellow)OPTIONAL
-        \(colorWhite)--curl-path: The path to Curl \(colorYellow)OPTIONAL
-        \(colorWhite)--ignore-versions: Ignores checking '.swiftTranslationsCodegen' version \(colorYellow)OPTIONAL
-        \(colorWhite)--default-language: Set's the default language in Localization.Language \(colorYellow)OPTIONAL
-        \(colorWhite)--exclude-objc-apis: Excludes features that depends on the Obj-c runtime \(colorYellow)OPTIONAL
-        """)
+    \(colorWhite)Hedvig Translations Codegen
+    
+    Arguments:
+    \(colorWhite)--projects: The projects you want to fetch translations for (for example: "[App, IOS]") \u{001B}[0;31mREQUIRED
+    \(colorWhite)--destination: Full path of desired destination for generated Swift file (including ".swift", for example "translations/translations.swift") \(colorRed)REQUIRED
+    \(colorWhite)--swiftformat-path: The path to the Swiftformat CLI \(colorYellow)OPTIONAL
+    \(colorWhite)--curl-path: The path to Curl \(colorYellow)OPTIONAL
+    \(colorWhite)--ignore-versions: Ignores checking '.swiftTranslationsCodegen' version \(colorYellow)OPTIONAL
+    \(colorWhite)--default-language: Set's the default language in Localization.Language \(colorYellow)OPTIONAL
+    \(colorWhite)--exclude-objc-apis: Excludes features that depends on the Obj-c runtime \(colorYellow)OPTIONAL
+    """)
     exit(1)
 }
 
@@ -139,9 +139,9 @@ guard let graphCMSRoot = graphCMSRoot else {
 
 func findReplacements(_ text: String) -> [String] {
     let range = NSRange(location: 0, length: text.utf16.count)
-    
+
     let results = placeholderRegex.matches(in: text, options: [], range: range)
-    
+
     return Array(Set(results.compactMap {
         String(text[Range($0.range, in: text)!])
     })).sorted { $0 < $1 }
@@ -151,33 +151,48 @@ func removeCurlyBraces(_ text: String, replaceOpeningWith: String = "", replaceC
     return text.replacingOccurrences(of: "{", with: replaceOpeningWith).replacingOccurrences(of: "}", with: replaceClosingWith)
 }
 
-/// removes curly braces from replacements
+extension String {
+    public func camelCased(with separator: Character) -> String {
+        if !contains(separator) {
+            return prefix(count).allSatisfy { char in char.isUppercase } ? lowercased() : self
+        }
+
+        return lowercased()
+            .split(separator: separator)
+            .enumerated()
+            .map { $0.offset > 0 ? $0.element.capitalized : $0.element.lowercased() }
+            .joined()
+    }
+}
+
+/// removes curly braces from replacements and camelCases them
 func cleanReplacements(_ replacements: [String]) -> [String] {
     return replacements
         .map { removeCurlyBraces($0) }
+        .map { $0.camelCased(with: "_") }
 }
 
 func indent(_ string: String, _ numberOfIndents: Int) -> String {
     var resultingString = "\(string)"
-    
+
     for _ in 0 ... numberOfIndents {
         resultingString = " \(resultingString)"
     }
-    
+
     return resultingString
 }
 
 func languageEnumCases() -> String {
     let cases = graphCMSRoot.data.languages.map { language -> String in
         let enumCase = indent("case \(language.code)", 6)
-        
+
         if language.code == graphCMSRoot.data.languages.last!.code {
             return enumCase
         }
-        
+
         return "\(enumCase)\n"
     }
-    
+
     return cases.joined(separator: "")
 }
 
@@ -186,28 +201,28 @@ func keysEnumCases() -> String {
         if key.translations.count == 0 {
             return false
         }
-        
+
         return true
     }.map { key -> String in
         print("\(colorGreen)Generating: \(key.value)\n")
         print("\(colorWhite)\(key.description ?? "")\n\n")
-        
+
         let description = key.description != nil ? "\(indent("/// \(key.description ?? "")", 6))\n" : ""
-        
+
         let replacementArguments = graphCMSRoot.data.languages.map { language -> [GraphCMSTranslation] in
             return language.translations.filter { $0.key?.value != nil }.filter { $0.key!.value == key.value }
-            }.flatMap { $0.map { findReplacements($0.text) } }.flatMap { $0 }
-        
+        }.flatMap { $0.map { findReplacements($0.text) } }.flatMap { $0 }
+
         if replacementArguments.count != 0 {
             let argumentNames = cleanReplacements(replacementArguments)
             let argumentNamesSyntax = Array(Set(argumentNames)).sorted { $0 < $1 }.map { "\($0): String" }.joined(separator: ", ")
-            
+
             return "\(description)\(indent("case \(key.value)(\(argumentNamesSyntax))", 6))"
         }
-        
+
         return "\(description)\(indent("case \(key.value)", 6))"
     }
-    
+
     return keys.joined(separator: "\n")
 }
 
@@ -216,66 +231,73 @@ func languageStructs() -> String {
         let switchStatementEnd = indent("}", 10)
         let switchStatement = indent("switch key {\n\(content)\n\(switchStatementEnd)", 10)
         return indent("""
-            static func `for`(key: Localization.Key) -> String {\n\(switchStatement)\n\(indent("}", 8))
-            """, 8)
+        static func `for`(key: Localization.Key) -> String {\n\(switchStatement)\n\(indent("}", 8))
+        """, 8)
     }
-    
+
     func getSwitchCases(_ language: GraphCMSLanguage) -> String {
         let switchCases = language.translations.filter { $0.key?.value != nil }.filter { translation in
             let key = graphCMSRoot.data.keys.first { key in key.value == translation.key!.value }
-            
+
             if key == nil {
                 print("\(colorYellow)WARNING \(colorWhite)hanging translation that is referencing key: \(colorYellow)\(translation.key!.value)\(colorWhite), it had the value: \(colorYellow)\"\(translation.text)\"\(colorWhite)\n")
                 return false
             }
-            
+
             return true
-            }.map { translation in
-                let replacements = cleanReplacements(findReplacements(translation.text))
-                var translationsRepoReplacements = replacements
-                    .map { name in "\"\(name)\": \(name)" }.joined(separator: ", ")
-                
-                if translationsRepoReplacements.count == 0 {
-                    translationsRepoReplacements = ":"
-                }
-                
-                let translationsRepoReturnStatement = indent("return text", 12)
-                let translationsRepoClosingBracket = indent("}", 10)
-                let translationsRepo = indent("""
-                    if let text = TranslationsRepo.findWithReplacements(key, replacements: [\(translationsRepoReplacements)]) {
-                    \(translationsRepoReturnStatement)
-                    \(translationsRepoClosingBracket)
-                    
-                    """, 10)
-                
-                let fallbackValue = indent("""
-                    return \"\"\"
-                    \(indent(removeCurlyBraces(translation.text, replaceOpeningWith: "\\(", replaceClosingWith: ")"), 10))
-                    \(indent("\"\"\"", 10))
-                    """, 10)
-                
-                let body = "\(translationsRepo)\n\(fallbackValue)"
-                
-                if replacements.count != 0 {
-                    let arguments = replacements.map { "\($0)" }.joined(separator: ", ")
-                    return indent("case let .\(translation.key!.value)(\(arguments)):\n\(body)\n", 12)
-                }
-                
-                return indent("case .\(translation.key!.value):\n\(body)\n", 12)
-            }.joined(separator: "").dropLast(1)
-        
+        }.map { translation in
+            let replacements = findReplacements(translation.text)
+            var translationsRepoReplacements = replacements
+                .map { removeCurlyBraces($0) }
+                .map { name in "\"\(name)\": \(name.camelCased(with: "_"))" }.joined(separator: ", ")
+
+            if translationsRepoReplacements.count == 0 {
+                translationsRepoReplacements = ":"
+            }
+
+            let translationsRepoReturnStatement = indent("return text", 12)
+            let translationsRepoClosingBracket = indent("}", 10)
+            let translationsRepo = indent("""
+            if let text = TranslationsRepo.findWithReplacements(key, replacements: [\(translationsRepoReplacements)]) {
+            \(translationsRepoReturnStatement)
+            \(translationsRepoClosingBracket)
+            
+            """, 10)
+
+            var fallbackInterpolation = removeCurlyBraces(translation.text, replaceOpeningWith: "\\(", replaceClosingWith: ")")
+
+            replacements.map { removeCurlyBraces($0) }.forEach {
+                fallbackInterpolation = fallbackInterpolation.replacingOccurrences(of: "\\(\($0))", with: "\\(\($0.camelCased(with: "_")))")
+            }
+
+            let fallbackValue = indent("""
+            return \"\"\"
+            \(indent(fallbackInterpolation, 10))
+            \(indent("\"\"\"", 10))
+            """, 10)
+
+            let body = "\(translationsRepo)\n\(fallbackValue)"
+
+            if replacements.count != 0 {
+                let arguments = cleanReplacements(replacements).map { "\($0)" }.joined(separator: ", ")
+                return indent("case let .\(translation.key!.value)(\(arguments)):\n\(body)\n", 12)
+            }
+
+            return indent("case .\(translation.key!.value):\n\(body)\n", 12)
+        }.joined(separator: "").dropLast(1)
+
         let defaultStatement = indent("default: return String(describing: key)", 12)
-        
+
         return "\(String(switchCases))\n\(defaultStatement)"
     }
-    
+
     let structs = graphCMSRoot.data.languages.map {
         indent("""
-            struct \($0.code) {
-            \(getStaticForFunc(getSwitchCases($0)))\n
-            """, 6)
-        }.map { "\($0)\(indent("}\n", 6))" }.joined()
-    
+        struct \($0.code) {
+        \(getStaticForFunc(getSwitchCases($0)))\n
+        """, 6)
+    }.map { "\($0)\(indent("}\n", 6))" }.joined()
+
     return String(structs.dropLast(1))
 }
 
@@ -283,7 +305,7 @@ func getLocalizationKeyReflection() -> String {
     if excludeObjcAPIs {
         return ""
     }
-    
+
     return """
     static var localizationKey: UInt8 = 0
 
@@ -362,4 +384,4 @@ swiftFormatTask.waitUntilExit()
 
 print("\(colorGreen)File generation completed!")
 print("\(colorWhite)")
-
+        
