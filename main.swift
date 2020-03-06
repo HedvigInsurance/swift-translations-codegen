260e00cddff9bb39ee01bc38b48079cf39ce7117
Sam Pettersson <sam@sampettersson.com>
2020-03-06 11:33:56 +0100

    Make init public

--- main.swift
+++ main.swift
@@ -387,7 +387,7 @@ extension Float: LocalizationStringConvertible {
 public extension String {
     \(getLocalizationKeyReflection())
 
-    init(key: Localization.Key, locale: Localization.Locale = Localization.Locale.currentLocale) {
+    public init(key: Localization.Key, locale: Localization.Locale = Localization.Locale.currentLocale) {
         switch locale {
             \(graphCMSRoot.data.languages.map { "case .\($0.code): self = Localization.Translations.\($0.code).for(key: key)" }.joined(separator: "\n"))
         }
